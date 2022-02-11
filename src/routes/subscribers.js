1c3de7ad7642dd46a53835d92c17a6d9736d1918
Grimstar15 <ttsegtsee@gmail.com>
2022-02-11 14:36:55 +0800

    database worked

--- src/routes/subscribers.js
+++ src/routes/subscribers.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const Subscriber = require("../models/subscriber");
 
-router.get("/", (req, res) => {
-  res.send("Hello World");
+router.get("/", async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find();
+    res.json(subscribers);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 router.get("/:id", (req, res) => {});
 
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscriberToChannel: req.body.subscriberToChannel,
+  });
+
+  try {
+    const newSubscriber = await subscriber.save();
+    res.status(201).json(newSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 router.patch("/:id", (req, res) => {});
 
