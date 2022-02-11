643da51802005212ce8a6cc828a3cbb1d6ae8f4d
Grimstar15 <ttsegtsee@gmail.com>
2022-02-11 16:39:38 +0800

    package.json change

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+
+app.use(bodyParser.json());
 
 mongoose.connect("mongodb://localhost/subtest", { useNewUrlParser: true });
 const db = mongoose.connection;
