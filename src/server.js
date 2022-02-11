cdf87b5145d4eae595aa22a3ddd92bccefb39b26
Grimstar15 <ttsegtsee@gmail.com>
2022-02-11 15:29:42 +0800

    database testing linux server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-require("dotenv").config();
+require("dotenv/config");
 
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/subtest", { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
