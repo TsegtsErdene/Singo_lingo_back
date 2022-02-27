8dcb7270e63c2404b0b6ca9137f8891e2c9552b7
Grimstar <ttsegtsee@gmail.com>
2022-02-27 18:33:16 +0800

    home router added

--- src/server.js
+++ src/server.js
@@ -8,18 +8,23 @@ const categoryRouter = require("./models/Category/CategoryController");
 const chapterRouter = require("./models/Chapter/ChapterController");
 const novelRouter = require("./models/Novel/NovelController");
 const userRouter = require("./models/User/UserController");
+const homeRouter = require("./models/Home/HomeController");
 
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.CUSTOMCONNSTR_MyConnectionString, {
-  useNewUrlParser: true,
-});
+mongoose.connect(
+  process.env.CUSTOMCONNSTR_MyConnectionString || "mongodb://localhost/novel",
+  {
+    useNewUrlParser: true,
+  }
+);
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
 
 app.use(express.json());
 
+app.use("/", homeRouter);
 app.use("/Bookmark", bookmarkRouter);
 app.use("/Category", categoryRouter);
 app.use("/Chapter", chapterRouter);
